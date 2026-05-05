e1c1ecfa5c513ee936527397226ad0f6b38a11a3
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-05 15:48:18 -0400

    Added variables and methods to hotel class

--- src/main/java/com/pluralsight/Hotel.java
+++ src/main/java/com/pluralsight/Hotel.java
@@ -9,5 +9,58 @@ public class Hotel {
         this.numberOfSuites = numberOfSuites;
         this.numberOfRooms = numberOfRooms;
     }
-    public Hotel()
+
+    public Hotel(String name, int numberOfSuites, int numberOfRooms, int bookedSuites, int bookedBasicRooms) {
+        this.name = name;
+        this.numberOfSuites = numberOfSuites;
+        this.numberOfRooms = numberOfRooms;
+        this.bookedSuites = bookedSuites;
+        this.bookedBasicRooms = bookedBasicRooms;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getNumberOfSuites() {
+        return numberOfSuites;
+    }
+
+    public int getNumberOfRooms() {
+        return numberOfRooms;
+    }
+
+    public int getBookedSuites() {
+        return bookedSuites;
+    }
+
+    public int getBookedBasicRooms() {
+        return bookedBasicRooms;
+    }
+
+    public int getAvailableSuites() {
+        return numberOfRooms - bookedBasicRooms;
+    }
+
+    public int getAvailableRooms() {
+        return numberOfSuites - bookedSuites;
+    }
+
+    public boolean bookRoom(int numberOfRooms, boolean isSuite) {
+        if (isSuite) {
+            if (getAvailableSuites() >= numberOfRooms) {
+                bookedSuites += numberOfRooms;
+                return true;
+            } else {
+                return false;
+            }
+        } else {
+            if (getAvailableRooms() >= numberOfRooms) {
+                bookedBasicRooms += numberOfRooms;
+                return true;
+            } else {
+                return false;
+            }
+        }
+    }
 }
