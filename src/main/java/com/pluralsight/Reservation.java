f4572c756feaa9e8dc73854ec32d3d477528a501
sourpunchlover <sndgpdesigns@gmail.com>
2026-05-05 10:57:11 -0400

    Added variables and constructor and getters to reservation class

--- src/main/java/com/pluralsight/Reservation.java
+++ src/main/java/com/pluralsight/Reservation.java
@@ -3,12 +3,54 @@ package com.pluralsight;
 public class Reservation {
     private String roomType;
     private int numberOfNights;
-    private boolean isWeekend;
+    private boolean weekend;
 
     public Reservation(String roomType, int numberOfNights, boolean isWeekend) {
         this.roomType = roomType;
         this.numberOfNights = numberOfNights;
-        this.isWeekend = isWeekend;
+        this.weekend = weekend;
+    }
+    public double getPrice() {
+        double price = 0;
+        if (this.roomType.equalsIgnoreCase("king")) {
+            price = 139.00;
+        } else  {
+            price = 124.00;
+        }
+
+        if (this.isWeekend()) {
+            price = price * 1.10; //increase by 10%
+        }
+
+        return price;
+    }
+
+    private double getReservationTotal() {
+        return this.getPrice() * this.numberOfNights;
+    }
+
+    public String getRoomType() {
+        return roomType;
+    }
+
+    public void setRoomType(String roomType) {
+        this.roomType = roomType;
+    }
+
+    public int getNumberOfNights() {
+        return numberOfNights;
+    }
+
+    public void setNumberOfNights(int numberOfNights) {
+        this.numberOfNights = numberOfNights;
+    }
+
+    public boolean isWeekend() {
+        return weekend;
+    }
+
+    public void setWeekend(boolean weekend) {
+        this.weekend = weekend;
     }
 
 
